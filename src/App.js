44b97532d1f5d1a7d8eb6bd9dac6590732d5c2e3
= <=>
2021-11-04 03:08:33 +0530

    Basic Dashboard

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { ThemeProvider } from "@emotion/react";
+import Dashboard from "./pages/Dashboard";
 function App() {
- 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{state}</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Dashboard />
+    </>
   );
 }
 
