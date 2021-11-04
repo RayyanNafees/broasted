f60acd0552e6b7980459a1ece0de34f734318428
= <=>
2021-11-04 17:43:49 +0530

    Organised dashboard

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { ThemeProvider } from "@emotion/react";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
 function App() {
   return (
     <>
