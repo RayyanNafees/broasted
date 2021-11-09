a338da2cef8c3648497db69a9a1be3c21a4318ef
= <=>
2021-11-09 12:04:25 +0530

    redux persistance added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,25 @@
 import { combineReducers, createStore } from "redux";
 import menu from "./menu";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 
 const rootReducer = combineReducers({
   menu,
 });
 
-const store = createStore(rootReducer);
+
+// STORE________________________
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const  store = createStore(persistedReducer)
 
 export default store;
+
+export const persistor = persistStore(store);
